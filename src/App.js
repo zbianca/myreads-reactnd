14943c3f3cc5955d8af4553eb8342453e1158a82
zbianca <bianca.zanardi@gmail.com>
2018-05-29 17:58:21 +0200

    config ESLint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import { Route } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
